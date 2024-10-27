1a2d0ecffc1989ca29340ffd0bd3df64676fb9a8
Uddyaloke <uddyaloke@gmail.com>
2024-10-27 19:01:03 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import { Metadata } from "next";
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+
+export const metadata: Metadata = {
+  title: "IAWIWB",
+  description: "Mind Coaching App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
